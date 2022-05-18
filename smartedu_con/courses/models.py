441ff26dc64af1e2e74ee963604b8641e1b0c583
tarikbugra <41157201+tarikbugra@users.noreply.github.com>
2022-05-18 04:52:22 +0300

    models were created!

--- smartedu_con/courses/models.py
+++ smartedu_con/courses/models.py
@@ -1,5 +1,16 @@
+from enum import unique
+from tabnanny import verbose
 from django.db import models
+from django.forms import CharField
+
 
 # Create your models here.
 class Course(models.Model):
-    pass
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='courses/%Y/%m/%d/', default='courses/CINA.png')
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
