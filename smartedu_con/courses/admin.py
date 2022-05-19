869c11dc692c383f44a1cbc093d3240cc1762203
tarikbugra <41157201+tarikbugra@users.noreply.github.com>
2022-05-19 03:08:37 +0300

    courses page was created

--- smartedu_con/courses/admin.py
+++ smartedu_con/courses/admin.py
@@ -3,4 +3,8 @@ from .models import Course
 
 
 # Register your models here.
-admin.site.register(Course)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'available')
+    list_filter = ('available',)
+    search_fields = ('name', 'description')
