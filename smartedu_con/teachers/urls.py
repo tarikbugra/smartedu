10bcab8b14a45b4bc6ae12f1b4f9bc5d11a162e5
tarikbugra <41157201+tarikbugra@users.noreply.github.com>
2022-05-20 21:21:45 +0300

    course and teacher pages were linked and teacher pages are created

--- smartedu_con/teachers/urls.py
+++ smartedu_con/teachers/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from teachers.views import TeacherView
+from teachers.views import TeacherListView, TeacherDetailView
 
 
 urlpatterns = [
-    path('', TeacherView.as_view(), name='teachers'),
+    path('', TeacherListView.as_view(), name='teachers'),
+    path('teachers/<int:pk>', TeacherDetailView.as_view(), name='teacher_detail'),
 ]
