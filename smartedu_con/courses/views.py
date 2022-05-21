fb087b3eed6f934e08904c28783935036597d550
tarikbugra <41157201+tarikbugra@users.noreply.github.com>
2022-05-21 04:43:15 +0300

    search area was worked

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Course, Category, Tag
 
 
 # Create your views here.
-def course_list(request):
-    courses = Course.objects.all().order_by('-date')
+def course_list(request, category_slug=None, tag_slug=None):
+    category_page = None
+    tag_page = None
+
     categories = Category.objects.all()
     tags = Tag.objects.all()
+
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        courses = Course.objects.filter(available=True, category=category_page)
+
+    elif tag_slug != None:
+        tag_page = get_object_or_404(Tag, slug=tag_slug)
+        courses = Course.objects.filter(available=True, tag=tag_page)
+
+    else:
+        courses = Course.objects.all().order_by('-date')
 
     context = {
         'courses': courses,
@@ -25,6 +38,35 @@ def course_detail(request, category_slug, course_id):
     }
 
     return render(request, 'course.html', context)
+
+
+
+def search(request):
+    courses = Course.objects.filter(name__contains=request.GET['search'])
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses': courses,
+        'categories': categories,
+        'tags': tags
+    }
+
+    return render(request, 'courses.html', context)
+
+
+"""def course_list(request):
+    courses = Course.objects.all().order_by('-date')
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses': courses,
+        'categories': categories,
+        'tags': tags
+    }
+
+    return render(request, 'courses.html', context)
 
 
 def category_list(request, category_slug):
@@ -52,4 +94,4 @@ def tag_list(request, tag_slug):
         'tags': tags
     }
 
-    return render(request, 'courses.html', context)
+    return render(request, 'courses.html', context)"""
